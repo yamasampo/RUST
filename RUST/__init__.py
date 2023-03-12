927d21c018c9fc1c6ca7801ea6f77529f8720b47
Jack Tierney <jack.tierney@student.ucc.ie>
2023-03-12 14:33:32 +0000

    dunderinit fix

--- RUST/__init__.py
+++ RUST/__init__.py
@@ -1,2 +1,5 @@
-from .main import *
-from .methods import *
+from RUST import methods
+from RUST import rust_amino
+from RUST import rust_tripeptide
+from RUST import rust_synergy
+from RUST import rust_plot
